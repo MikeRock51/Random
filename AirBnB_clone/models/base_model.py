9ae80abbf669838c4425db9b84cbbaf06ee5e21b
MikeRock51 <mikerockmusic51@gmail.com>
2023-04-22 17:54:10 +0100

    Create new instance from dict

--- AirBnB_clone/models/base_model.py
+++ AirBnB_clone/models/base_model.py
@@ -9,11 +9,21 @@ from datetime import datetime
 class BaseModel:
     """Defines all common attributes and methods for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Object contructor"""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return (f"[{type(self).__name__}] ({self.id}) {self.__dict__}")
@@ -23,6 +33,7 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """Generates a dictionary representation of an instance"""
         instance = self.__dict__
         instance['__class__'] = type(self).__name__
         instance['created_at'] = instance['created_at'].isoformat()
