7f07e7ca73d1f0293874caeed2109f04283ab89b
MikeRock51 <mikerockmusic51@gmail.com>
2023-04-27 07:58:22 +0100

    Setup DBStorage init method

--- AirBnB_clone/models/engine/db_storage.py
+++ AirBnB_clone/models/engine/db_storage.py
@@ -2,6 +2,7 @@
 """Defines the database storage engine for the program"""
 
 from sqlalchemy import create_engine
+from os import getenv
 
 class DBStorage:
     """DB storage engine class"""
@@ -9,5 +10,11 @@ class DBStorage:
     __session = None
 
     def __init__(self):
-        self.__engine = create_engine(
+        user = getenv('HBNB_MYSQL_USER')
+        host = getenv('HBNB_MYSQL_HOST')
+        pwd = getenv('HBNB_MYSQL_PWD')
+        db = getenv('HBNB_MYSQL_DB')
+        self.__engine = create_engine("mysql+mysqldb//{}:{}@{}/{}".format(user, pwd, host, db), pool_pre_ping=True)
+
+        if HBNB_ENV == 'test':
 
