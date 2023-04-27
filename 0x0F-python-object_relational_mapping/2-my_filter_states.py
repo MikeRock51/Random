dbb2b9079e87b81bc7b15bbd1d1992ead06de002
MikeRock51 <mikerockmusic51@gmail.com>
2023-04-27 09:46:52 +0100

    Add Task 2 ORM

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Lists all states from a database that matches a given name"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    username = argv[1]
+    pwd = argv[2]
+    database = argv[3]
+    searchedState = argv[4]
+
+    conn = MySQLdb.connect(user=username, passwd=pwd, db=database,
+                                    host='localhost', charset='utf8')
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM states WHERE name =\
+                '{searchedState}' ORDER BY states.id")
+    query = cur.fetchall()
+
+    for row in query:
+        print(row)
