dbb0f27178114fbb8dad200bde201cb8f6dc66e7
MikeRock51 <mikerockmusic51@gmail.com>
2023-04-27 14:32:29 +0100

    Working on model_state ORM ; Low Battery

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a state model"""
+
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, Interger
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """A state class"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True, unique=True)
+    name = Column(String(128), nullable=False)
+
