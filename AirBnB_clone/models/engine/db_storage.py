f883be6883ff09b6c5c8a551b3cf41ddc9d4bb95
MikeRock51 <mikerockmusic51@gmail.com>
2023-04-28 16:21:05 +0100

    Add DbStorage reload method

--- AirBnB_clone/models/engine/db_storage.py
+++ AirBnB_clone/models/engine/db_storage.py
@@ -4,6 +4,14 @@
 from sqlalchemy import create_engine
 from os import getenv
 from models.base_model import Base
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
 
 class DBStorage:
     """DB storage engine class"""
@@ -21,6 +29,7 @@ class DBStorage:
             Base.metadata.drop_all(engine)
 
     def all(self, cls=None):
+        """Returns a dictionary of all objects from the database"""
         objects = {}
         if cls is not None:
             queryResults = self.__session.query(cls).all()
@@ -34,5 +43,22 @@ class DBStorage:
                 for result in queryResult:
                     key = "{}.{}".format(result.__class__.__name__, result.id)
                     objects[key] = result
+
         return objects
 
+    def new(self, obj):
+        """Adds obj to the current database session"""
+       self.__session.add(obj)
+
+    def delete(self, obj=None):
+        """Deletes obj from the current database session"""
+        if obj is not None:
+            self.__session.query(obj.__class__.__name__).delete()
+
+    def reload(self):
+        """Creates all tables in the database"""
+        Base.metadata.create_all(engine)
+
+        session_factory = sessionmaker(bind=engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
