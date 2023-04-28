73d192932ce035e8120666c7942e063bed775c4b
MikeRock51 <mikerockmusic51@gmail.com>
2023-04-28 09:33:35 +0100

    Add Task 15 ORM

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a state model"""
+
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import Column, String, Integer
+
+Base = declarative_base()
+
+
+class State(Base):
+    """A state class"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False,
+                                autoincrement=True, unique=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='all, delete')
