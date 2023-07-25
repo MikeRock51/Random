8c358b47c7db7912c8525dba39e41610065e13e7
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-25 06:47:22 +0100

    Add intersection solution

--- interSection.c
+++ interSection.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -5,25 +7,78 @@
  *     struct ListNode *next;
  * };
  */
+typedef struct ListNode ListNode;
+
 struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB)
 {
-	ListNode *cursorA = headA;
-	ListNode *cursorB = headB;
-	int l1_len = 0;
-	int l2_len = 0;
-	
+    ListNode *cursorA = headA;
+    ListNode *cursorB = headB;
+    ListNode *tailA = NULL;
+    ListNode *tailB = NULL;
+    int lA_len = 0;
+    int lB_len = 0;
+    int diff = 0;
 
-	while (cursorA != NULL || cursorB != NULL)
-	{
-		if (cursorA != NULL)
-		{
-			l1_len++;
-			cursorA = cursorA->next
-		}
-		if (cursorB != NULL)
-		{
-			l2_len++;
-			cursorB = cursorB->next
-		}
-	}
+    if (headA == NULL || headB == NULL)
+    {
+        return NULL;
+    }
+
+    while (cursorA != NULL || cursorB != NULL)
+    {
+        if (cursorA != NULL)
+        {
+            lA_len++;
+            if (cursorA->next == NULL)
+                tailA = cursorA;
+            cursorA = cursorA->next;
+        }
+        if (cursorB != NULL)
+        {
+            lB_len++;
+            if (cursorB->next == NULL)
+                tailB = cursorB;
+            cursorB = cursorB->next;
+        }
+    }
+    // printf("Here\n");
+    // printf("%p, %p\n", tailA, tailB);
+
+    if (tailA != tailB)
+    {
+        return NULL;
+    }
+
+    cursorA = headA;
+    cursorB = headB;
+
+    if (lA_len > lB_len)
+    {
+        diff = lA_len - lB_len;
+        while (diff > 0)
+        {
+            cursorA = cursorA->next;
+            diff--;
+        }
+    }
+
+    if (lB_len > lA_len)
+    {
+        diff = lB_len - lA_len;
+        while (diff > 0)
+        {
+            cursorB = cursorB->next;
+            diff--;
+        }
+    }
+
+    while (cursorA != NULL && cursorB != NULL)
+    {
+        if (cursorA == cursorB)
+            return cursorA;
+        cursorA = cursorA->next;
+        cursorB = cursorB->next;
+    }
+
+    return NULL;
 }
